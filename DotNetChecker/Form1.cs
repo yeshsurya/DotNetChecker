6df56084c0d8f655345c93bc2d322fd36d56c2dc
Yeshwanth Nagaraj <ynagaraj@aptean.com>
2017-09-26 09:56:10 +0530

    First Files

---
+++ DotNetChecker/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Text;
+
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DotNetChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<String> ver = GetVersionFromRegistry();
+            foreach(var ele in ver)
+            {
+                listBox1.Items.Add(ele);
+            }
+        }
+        private static List<String> GetVersionFromRegistry()
+        {
+            List<String> versions = new List<String>();
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "") //no install info, must be later.
+                            Console.WriteLine(versionKeyName + "  " + name);
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                                Console.WriteLine(versionKeyName + "  " + name);
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    //Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                    versions.Add("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    //Console.WriteLine("  " + subKeyName + "  " + name);
+                                    versions.Add("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return versions;
+        }
+    }
+}
